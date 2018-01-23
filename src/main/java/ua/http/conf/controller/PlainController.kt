4c2f86a68fe584a65ccb81abad7d33933edde6c0
Andrii Bezruchko <abezruchko@corelogic.com>
2018-01-23 18:26:04 +0200

    Add Push functionality

--- src/main/java/ua/http/conf/controller/PlainController.kt
+++ src/main/java/ua/http/conf/controller/PlainController.kt
@@ -14,10 +14,17 @@ class PlainController {
     fun sample(req: HttpServletRequest, resp: HttpServletResponse): ModelAndView {
         val pushBuilder = req.newPushBuilder()
         pushBuilder?.apply {
-            pushBuilder.path("res/images/logo.png").push()
-            pushBuilder.path("res/images/background.jpg").push()
+//            pushBuilder.path("res/images/logo.png").push()
+//            pushBuilder.path("res/images/background.jpg").push()
+//            pushBuilder.path("res/images/header_1.jpg").push()
+
             pushBuilder.path("res/css/style.css").push()
-            pushBuilder.path("res/images/header_1.jpg").push()
+            for (i in 1..400) {
+                pushBuilder
+                        .path("res/images/tiger/tiger_$i.jpg")
+                        .addHeader("content-type", "image/jpg")
+                        .push()
+            }
         }
         return ModelAndView("main2")
     }
